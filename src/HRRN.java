477e3eb4a9f0b7612479730f916542c9ed5f7d9a
chan-Yung-Joo <jcy00110066@kakao.com>
2022-05-24 00:29:00 +0900

    6차 수정(교재 예제는 맞게 나오나 다른 예제는 틀림, 추가 수정 필요)

--- src/HRRN.java
+++ src/HRRN.java
@@ -1,4 +1,9 @@
 import java.util.*;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 <체크 해야하는 것들>
@@ -46,12 +51,12 @@ class Process_Info {
         }
     };
 
-    public int calc_WaitTime(int curTime) {
-        return (curTime - ServiceTime);
+    public double calc_waitTime(int curTime){
+        return (curTime - ArriveTime);
     }
 
     public double calc_RatioTime(int curTime){
-        return (double)((calc_WaitTime(curTime) + ServiceTime) / (ServiceTime));
+        return (double)((calc_waitTime(curTime) + ServiceTime) / (ServiceTime));
     }
 
 }
@@ -61,27 +66,74 @@ public class HRRN {
     public static Queue<Process_Info> readyQueue = new LinkedList<>();
     //public int num_Process = 0; // 입력된 프로세스의 개수를 저장하는 변수
 
-    public static void main(String[] args) {
-        int [] process_id = {1,2,3,4,5,0};
-        int [] arrive_time = {0,2,4,6,8};
-        int [] service_time = {3,6,4,5,2};
+    public static void main(String[] args) throws FileNotFoundException {
 
-        //int [] process_id = {3,2,5,1,4,0};
-        //int [] arrive_time = {4,2,8,0,6};
-        //int [] service_time = {4,6,2,3,5};
+        List<String> input = new ArrayList<String>(); // 파일에서 데이터를 읽어올 공간(문자열로 읽기)
+        List<Integer> arrange_input = new ArrayList<Integer>(); // 문자열로 읽은 데이터를 ','를 제거하고 저장하기 위한 공간
+        Scanner scanner = new Scanner(new File("/Users/chanyungjoo/Desktop/School/2022-1학기/SW/OS_Team_Project/src/input.txt"));
+
+
+        // 데이터를 줄 단위로 String 타입의 형태로 읽어와 저장
+        while(scanner.hasNext()) {
+            String str = scanner.next();
+            input.add(str);
+        }
+
+        // 그 저장된 데이터들을 꺼내서 문자열 순회를 통해 ','를 제거하고 arrange_input에 저장
+        for(String data : input) {
+            for (int i = 0; i < data.length(); i++) {
+                char obs = data.charAt(i);
+                if (obs == ',') { continue; }
+                else { arrange_input.add(Integer.parseInt(Character.toString(obs)));}
+            }
+        }
+
+        List<Integer> process_id = new ArrayList<Integer>();
+        List<Integer> arrive_time = new ArrayList<Integer>();
+        List<Integer> service_time = new ArrayList<Integer>();
+
+        /*
+        현재 arrange_input에는 데이터가 다음과 같은 순서로 저장이 되어 있을 것임.
+        >> [PID, Arrive_Time, Service_Time,PID, Arrive_Time, Service_Time,PID, Arrive_Time, Service_Time ....] <<
+        PID, Arrive Time, Service Time을 분리해서 새로운 List에 저장하기 위해서 나머지 연산을 사용했음.
+        반복 변수(i)를 3으로 나누었을 때 나머지가 0이라는 것은 PID를 의미하고
+        반복 변수(i)를 3으로 나누었을 때 나머지가 0이라는 것은 Arrive Time을 의미하고
+        반복 변수(i)를 3으로 나누었을 때 나머지가 0이라는 것은 Service Time을 의미한다.
+         */
+
+        Input_Loop: for(int i=0; i<arrange_input.size(); i++) {
+            if(i%3 == 0) {
+                if (arrange_input.get(i) == 0) {
+                    System.out.println("Process ID는 0이 불가합니다.");
+                    break Input_Loop;
+                }
+                else if (arrange_input.get(i) < 0 || arrange_input.get(i) > 99) {
+                    System.out.println("Out of Range (Process ID)");
+                    break Input_Loop;
+                }
+                else { process_id.add(arrange_input.get(i)); }
+            }
+
+            else if(i%3 == 1){
+                arrive_time.add(arrange_input.get(i));
+            }
+            else if(i%3 == 2) {
+                service_time.add(arrange_input.get(i));
+            }
+        }
 
         int num_Process = 0; // 입력된 프로세스의 개수를 저장하는 변수
 
-        /*
+
         System.out.println("----------------------------------------------------------------");
         System.out.println("Input the Process");
         System.out.println("PID\t\tArrive Time\t\tService Time");
-        for(int i=0; i< process_id.length-1; i++){
-            System.out.println(" " + process_id[i] + "\t\t\t" + arrive_time[i] +
-                    "\t\t\t\t   " + service_time[i]);
+        for(int i=0; i< process_id.size(); i++){
+            System.out.println(" " + process_id.get(i) + "\t\t\t" + arrive_time.get(i) +
+                    "\t\t\t\t   " + service_time.get(i));
         }
         System.out.println("----------------------------------------------------------------");
-        */
+
 
         // 프로세스 Input을 받아와 Arrive Time(도착 시간) 기준으로 정렬하여 큐에 저장 -> 우선순위 큐 이용
 
@@ -95,36 +147,24 @@ public class HRRN {
         });
 
 
-        System.out.println("프로세스 입력 확인용 출력");
         System.out.println("----------------------------------------------------------------");
-        for (int i=0; i<process_id.length; i++){
-            if (process_id[i] < 0 || process_id[i] > 99) {
-                System.out.println("There is wrong Process ID (Out of PID's Range)");
-                continue;
-            }
-            if (process_id[i] == 0){
-                System.out.println("Stopping Input");
-                System.out.println();
-                break;
-            }
-            else {
-                Process_Info ps = new Process_Info(process_id[i], arrive_time[i], service_time[i]);
+        for (int i=0; i<process_id.size(); i++){
 
-                System.out.printf("Input the Process %d", i+1);
-                System.out.println();
+            Process_Info ps = new Process_Info(process_id.get(i), arrive_time.get(i), service_time.get(i));
 
-                processQueue.add(ps);
-                num_Process++;
-                System.out.println("Add the Process into the Queue.");
-                System.out.println();
-            }
+            System.out.printf("Input the Process %d", i+1);
+            System.out.println();
+
+            processQueue.add(ps);
+            num_Process++;
+            System.out.println("Add the Process into the Queue.");
+            System.out.println();
+
         }
         System.out.printf("Num of Process : %d\n", num_Process);
 
-        /*
-        System.out.println("----------------------------------------------------------------");
 
-        System.out.println("Sorting the Queue");
+        System.out.println("----------------------------------------------------------------");
 
         System.out.println("Print the Process in the Queue");
         System.out.println();
@@ -135,19 +175,19 @@ public class HRRN {
                     "\t\t\t\t   " + process.getServiceTime());
         }
         System.out.println("----------------------------------------------------------------");
-        */
+
 
         // 시간(초)를 의미하는 변수
         // 프로세스를 스케줄링 하기 전, ReadyQueue에 미리 넣기 위함 (사실은 비선점 모드라서 미리 가능한 일이라 생각이 듦)
         // 스케줄링 시작하기 전을 의미하기 위해 기본값으로 -1로 세팅을 함
-        // int time = -1;
+        int time = -1;
 
         // Arrive Time 순으로 정렬된 프로세스들을 ReadyQueue로 넣어주는 작업
         // processQueue에서 peek 연산 후에 readyQueue에 대입 후 processQueue.poll을 통해 삭제
         // 최종적으로 readyQueue에 Process가 도착한 시간 순으로 정렬되어 저장되어 있고
         // processQueue는 poll 연산을 모두 진행했으므로 비어있는 상태가 될 것이다.
 
-        /*
+
         while(true) {
             ++time;
            if(processQueue.isEmpty()){ break; }
@@ -161,17 +201,16 @@ public class HRRN {
                }
            }
         }
-        */
 
-        /*
-        System.out.println("Ready Queue Information");
+
+
+        /* System.out.println("Ready Queue Information");
         System.out.println("PID\t\tArrive Time\t\tService Time");
         for (Process_Info process : readyQueue){
             System.out.println(" " + process.getProcessID() + "\t\t\t" + process.getArriveTime() +
                     "\t\t\t\t   " + process.getServiceTime());
         }
-        System.out.println("----------------------------------------------------------------");
-        */
+        System.out.println("----------------------------------------------------------------");*/
 
         // 시간(초)를 의미하는 변수
         // 프로세스 스케줄링에 있어서 현재 시간이 필요함
@@ -181,9 +220,48 @@ public class HRRN {
         //Process_Info curPs = readyQueue.peek();
         //System.out.printf("%d, %d, %d\n", curPs.getProcessID(), curPs.getArriveTime(), curPs.getServiceTime());
 
+        Process_Info curPs = null;
+        Process_Info nextPs = null;
+
         // 본격적으로 readyQueue에서 Process를 스케줄링을 진행할 부분
-        while(true) {
+
+        while(num_Process!=0) {
             ++real_time;
+            if(curPs != null) {
+                ++curPs.ExecTime;
+
+                for(var _process: readyQueue) { _process.calc_waitTime(real_time); }
+
+                if(curPs.checkFinish()) {
+
+                    System.out.printf(">>>> %d초에 수행 완료\t\t", real_time);
+                    // 정규화된 반환 시간 = 반환시간/실행시간
+                    System.out.printf("PID : %2d\tArrive Time: %2d\t,Service Time: %2d\tReturn Time: %2d\t\tNormalized Return Time: %.3f",
+                            curPs.getProcessID(), curPs.getArriveTime(), curPs.getServiceTime(), real_time, (double)real_time/curPs.getServiceTime());
+                    System.out.println();
+
+                    readyQueue.remove(curPs);
+                    --num_Process;
+                    curPs = null;
+
+                }
+
+            }
+
+            nextPs = readyQueue.peek();
+
+            for (var obs : readyQueue) {
+                if (obs.calc_RatioTime(real_time) > nextPs.calc_RatioTime(real_time)) {
+                    nextPs = obs;
+                }
+            }
+            curPs = nextPs;
+
+
+
+
+
+            /*
             for (int i = 0; i < processQueue.size(); i++) {
                 Process_Info ps = processQueue.peek();
                 if (ps.getArriveTime() == real_time) {
@@ -196,6 +274,7 @@ public class HRRN {
             //System.out.println(readyQueue.size());
 
             Process_Info curPs = readyQueue.peek();
+            ++curPs.ExecTime;
 
             if (curPs.checkFinish()) { // 현재 프로세스가 수행을 완료했을 경우
 
@@ -208,24 +287,22 @@ public class HRRN {
                 num_Process--;
 
             }
+            else {
+                Process_Info nextPs = readyQueue.peek();
 
-            Process_Info nextPs = readyQueue.peek();
 
-
-            //curPs.ExecTime++;
-            for (var ps : readyQueue) {
-                if (ps.calc_RatioTime(real_time) > nextPs.calc_RatioTime(real_time)) {
-                    nextPs = ps;
+                //curPs.ExecTime++;
+                for (var ps : readyQueue) {
+                    if (ps.calc_RatioTime(real_time) > nextPs.calc_RatioTime(real_time)) {
+                        nextPs = ps;
+                    }
                 }
                 curPs = nextPs;
-            }
-            //curPs.ExecTime++;
+                //curPs.ExecTime++;
+            } // else 문 */
 
+        }
 
-
-
-
-        } // while문
     } // void main 함수
 } // class HRRN
 
